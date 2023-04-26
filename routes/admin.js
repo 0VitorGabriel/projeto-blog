a3f3b483192b05eb21a4046ff7d880d864a6aa0c
0VitorGabriel <victoropo64@outlook.com>
2023-04-26 08:58:02 -0400

    formulario de cadastro de categorias

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (request, response) => {
-    response.render('admin/index.handlebars')
+    response.render('admin/index')
 })
 
 router.get('/posts', (request, response) => {
@@ -10,7 +10,11 @@ router.get('/posts', (request, response) => {
 })
 
 router.get('/categorias', (request, response) => {
-    response.send('pagina de categorias')
+    response.render('admin/categorias')
+})
+
+router.get('/categorias/add', (request, response) => {
+    response.render('admin/add_categorias')
 })
 
 module.exports = router
