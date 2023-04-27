d27ac917cd9e07683a62610d8991a11812dc5d7c
0VitorGabriel <victoropo64@outlook.com>
2023-04-27 18:33:57 -0400

    categorias salvas no mongodb

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+require('../models/Categoria')
+const categoria = mongoose.model('categorias')
 
 router.get('/', (request, response) => {
     response.render('admin/index')
@@ -11,6 +14,20 @@ router.get('/posts', (request, response) => {
 
 router.get('/categorias', (request, response) => {
     response.render('admin/categorias')
+})
+
+router.post('/categorias/nova', (request, response) => {
+    const nova_categoria = {
+        nome: request.body.nome,
+        slug: request.body.slug
+    }
+
+    new categoria(nova_categoria).save()
+    .then(() => {
+        console.log('categoria salva')
+    }).catch((err) => {
+        console.log('categoria não salva' + err)
+    })
 })
 
 router.get('/categorias/add', (request, response) => {
