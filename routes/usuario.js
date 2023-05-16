10a8d3c674b889f78efc482ecb71cffbaa1fdf78
0VitorGabriel <victoropo64@outlook.com>
2023-05-16 09:11:09 -0400

    validação do formulario de registro de usuarios

--- routes/usuario.js
+++ routes/usuario.js
@@ -9,4 +9,36 @@ router.get('/registro', (request, response) => {
     response.render('usuarios/registro')
 })
 
+router.post('/registro', (request, response) => {
+    var erros = []
+
+    if (!request.body.nome || request.body.nome == undefined || request.body.nome == null) {
+        erros.push({texto: 'nome inválido'})
+    }
+
+    if (!request.body.email || request.body.email == undefined || request.body.email == null) {
+        erros.push({texto: 'email inválido'})
+    }
+
+    if (!request.body.senha || request.body.senha == undefined || request.body.senha == null) {
+        erros.push({texto: 'senha inválido'})
+    }
+
+    if (request.body.senha.length < 8) {
+        erros.push({texto: 'senha muito curta'})
+    }
+
+    if (request.body.senha != request.body.senha_repetida) {
+        erros.push({texto: 'As senhas são diferentes'})
+    }
+
+    if (erros.length > 0) {
+
+        response.render('usuarios/registro', {erros: erros})
+
+    } else {
+
+    }
+})
+
 module.exports = router
